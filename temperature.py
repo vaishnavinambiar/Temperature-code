9d34b35dc4500b6b521ed1e5a42415837f31d522
vaishnavinambiar <45651562+vaishnavinambiar@users.noreply.github.com>
2019-01-25 16:00:49 +0530

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -5,16 +5,15 @@ month = int(raw_input("Enter the month "))
 day = int(raw_input("Enter the day "))
 date = datetime.date(year, month, day)
 date_2 = datetime.date(year, month, day)
-temp=input("Enter the temperatures: ")
 result=input("Type C for convert to Celsius and F for Fahrenheit:")
 my_dict = {}
 
-def temperature():
+def main():
     """ This function enters the temperatures and converts to farehneit or celcius. """
     for temp in range(3):
         temp=input("Enter the temperatures: ")
         temp=float(temp)
-        
+        user_input(temp)
         if result == 'C':
             convert= (9*temp+32*5)/5
             print("F= ", convert)
@@ -24,7 +23,7 @@ def temperature():
         else:
             print("Please insert correct syntax")
 
-def user_input():
+def user_input(temp):
     """ Intakes the user's temperature values and stores as in dict."""
     temperatures = []
     temperatures.append(temp)
@@ -44,7 +43,7 @@ def user_date():
     return date_2
         
         
-def display_temp():
+def min_max():
     """ Displays the minimum and maximum temperature from the user's dates as per the choice given."""
     choice_list=[1,2]
     for user_input in range(2):
@@ -54,13 +53,16 @@ def display_temp():
         elif(user_input==choice_list[1]):
             print min(my_dict[date][0])
 
-    
+def average():
+    """ Finds the average of the user's input temperatures."""
+    average = (temperatures[0]+ temperatures[1]+temperatures[2])/3
+    print ("Average is %d" %average)    
         
-
-temperature()
-user_input()
-user_date()
-display_temp()
+if __name__ == "__main__":
+    main()
+    user_date()
+    min_max()
+    average()
 
     
 
