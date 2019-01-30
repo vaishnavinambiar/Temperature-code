9dbdfc552797553e62b4d9c09663d1af348f214f
vaishnavinambiar <45651562+vaishnavinambiar@users.noreply.github.com>
2019-01-30 16:34:22 +0530

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -4,7 +4,6 @@ year = int(input("Enter the year "))
 month = int(input("Enter the month "))
 day = int(input("Enter the day "))
 date = datetime.date(year, month, day)
-
 my_dict = {}
 temperatures = []
 value = []
@@ -30,7 +29,6 @@ def get_conversion(temp,result):
 
 def get_user_date(temp):
     """ Intakes the user's temperature values and stores as in dict."""
-    
     temperatures.append(temp)
     key = date
     my_dict.setdefault(date,[])
@@ -38,10 +36,9 @@ def get_user_date(temp):
     my_dict[date] = [temperatures]
     return my_dict
 
-def get_min_max(value):
-    """ Displays the minimum and maximum temperature."""
-    return ("maximum value is",max(value))
-    return ("minimum value is",min(value))
+def get_minimum_maximum(value):
+    min_max = {'minimum': min(value), 'maximum' : max(value)}
+    return min_max
 
 def get_average(value):
     """ Finds the average."""
@@ -51,9 +48,7 @@ if __name__ == "__main__":
     user_value=get_user_temperature()
     get_user_date(user_value)
     print my_dict
-    get_min_max(value)
-    print("maximum value is",max(value))
-    print("minimum value is",min(value))
+    print get_minimum_maximum(value)
     print get_average(value)
         
 
